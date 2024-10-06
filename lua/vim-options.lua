11c1051c02f166e15457aae0870c49be9bf0c8b9
boyd <boyd@lloydshark.net>
2024-10-06 21:59:03 +1100

    Added more global settings. - Add line numbering - Window splitting - Pane navigation - Prevent auto start of conjure repl

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,5 +2,12 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
 vim.g.mapleader = " "
-
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>", {})
+vim.keymap.set("n", "<leader>h", ":split<CR>", {})
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", {})
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", {})
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", {})
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", {})
+vim.g["conjure#client_on_load"] = false
